f2a80c51425a380a79d111ffe101a790ce6b00c4
Callmerev95 <rchivas109@gmail.com>
2024-03-04 06:00:23 +0800

    update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,36 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#f8f7f4",
+        secondary: "#ffda79",
+        tertiary: "#191d2b",
+        slate: {
+          10: "#f1f3f4",
+        },
+        green: {
+          50: "#30af5b",
+          90: "#292c27",
+        },
+        gray: {
+          10: "#eeeeee",
+          20: "#a2a2a2",
+          30: "#7b7b7b",
+          50: "#5b5b5b",
+          90: "#141414",
+        },
+      },
+      fontFamily: {
+        nunito: ["var(--font-nunito)"],
+      },
+      screens: {
+        xs: "400px",
+        "3xl": "1680px",
+        "4xl": "2200px",
+      },
+      maxWidth: {
+        "10xl": "1512px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
