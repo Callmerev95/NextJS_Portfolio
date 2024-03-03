284ed9fa5fca2da84a84302bdc4724a97348e8b7
Callmerev95 <rchivas109@gmail.com>
2024-03-04 06:05:18 +0800

    initial commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      {/* logo */}
+      <Link href={"/"}>
+        <Image src={"./logo.svg"} alt="logo" height={99} width={99}></Image>
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
