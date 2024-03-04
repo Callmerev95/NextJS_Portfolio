b90a5347d01246c94b135eeb382ee99d7e97ef1c
Callmerev95 <rchivas109@gmail.com>
2024-03-04 18:31:07 +0800

    Create data for Nav

---
+++ constant/data.js
@@ -0,0 +1,38 @@
+// icons
+import {
+  HiHome,
+  HiUser,
+  HiSquare2Stack,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from "react-icons/hi2";
+
+import { FaBug, FaHtml5, FaCss3, FaJs, FaReact, FaFigma } from "react-icons/fa";
+
+export const LINKS = [
+  {
+    title: "home",
+    icon: <HiHome />,
+    path: "/",
+  },
+  {
+    title: "about",
+    icon: <HiUser />,
+    path: "/about",
+  },
+  {
+    title: "work",
+    icon: <HiSquare2Stack />,
+    path: "/work",
+  },
+  {
+    title: "testimonials",
+    icon: <HiChatBubbleBottomCenterText />,
+    path: "/testimonials",
+  },
+  {
+    title: "contact",
+    icon: <HiEnvelope />,
+    path: "/contact",
+  },
+];
